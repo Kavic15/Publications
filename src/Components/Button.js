e9fe93da95657c89959e3820f2487a742c5c6e4f
Kavic15 <kavatomas15@seznam.cz>
2023-04-17 11:09:28 +0200

    Button added handleClick

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function MyButton(props) {
+  function handleClick() {
+    alert(props.label + ' clicked!');
+  }
   return (
     <div>
-        <button onClick={props.onClick} className='btn btn-sm btn-outline-primary'>
+        <button onClick={handleClick} className='btn btn-sm btn-outline-primary'>
             {props.label}
         </button>
     </div>
